2aa23a2129781989cab37ce21aa81ff5ff8be6c4
Daniel <daniel.fb.jr@gmail.com>
2022-08-17 21:53:16 -0300

    Aula 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//  Aula 1
+//  int = Inteiro
+//  Criando uma variável
+int a;
+
+//  Escreve na tela
+Console.Write("Digite o valor de A: ");
+
+
+//  String = Letras ou palavras
+//  Console.ReadLine() = Ler a informação do usuario 
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Valor de a = ");
+Console.Write(a);
