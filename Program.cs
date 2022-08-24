efcccb613fbfaa2ba7c4faf775fad47db87a6c8d
Daniel <daniel.fb.jr@gmail.com>
2022-08-24 20:52:16 -0300

    Aula 2

--- Program.cs
+++ Program.cs
@@ -2,13 +2,13 @@
 //  int = Inteiro
 //  Criando uma variável
 /*
-    int     = inteiro
-    float   = real
-    double  = real
-    decimal = real
+    int     = inteiro   32bits
+    float   = real  32bits
+    double  = real  64bits
+    decimal = real  92bits
 
 */
-float salario;
+double salario;
 
 //  Escreve na tela
 Console.Write("Digite o seu salário: ");
@@ -16,8 +16,7 @@ Console.Write("Digite o seu salário: ");
 
 //  String = Letras ou palavras
 //  Console.ReadLine() = Ler a informação do usuario 
-salario = Convert.ToInt32(Console.ReadLine());
+salario = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Valor em R$ = ");
-Console.Write(salario);
-
+Console.Write("Valor em R$ = {0}",salario.ToString("0.00"));
+//  salario.ToString("0.00") = Definir padrão de exibição
