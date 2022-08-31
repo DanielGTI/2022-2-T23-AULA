63ec28103767bb032deb43d7c58f0d8341dfb13d
Daniel <daniel.fb.jr@gmail.com>
2022-08-31 20:21:31 -0300

    Ajuste do prog 3

--- Program.cs
+++ Program.cs
@@ -1,12 +1,46 @@
-﻿// EX 1000
-//Console.Write("Hello World!\n");
+﻿static void p1001(){
+    // EX 1000
+    //Console.Write("Hello World!\n");
 
-int a, b, x;
+    int a, b, x;
 
-a = Convert.ToInt32(Console.ReadLine());
-b = Convert.ToInt32(Console.ReadLine());
+    a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
 
-x = a + b;
+    x = a + b;
 
-Console.Write("X = {0}\n", x);
-//Console.Write("O valor da soma é = {0}\n", x);
+    Console.Write("X = {0}\n", x);
+    //Console.Write("O valor da soma é = {0}\n", x);
+}
+
+static void p1002(){
+
+    Console.Write("OK");
+}
+
+
+//  INICIO DO PROGRAMA
+
+    
+    int prog;
+    Console.Clear();
+    do{
+        
+        Console.Write("\nGerenciador de programas:\n");
+        Console.Write("\n\t| 0 para sair |\nDigite o programa desejado: ");
+        prog = Convert.ToInt32(Console.ReadLine());
+        if ( prog == 0 ){
+            Console.Write("\nObrigado por utilizar nossos programas.\n");
+            break;
+        }
+
+        switch ( prog ){
+            case 1001:  p1001();    break;
+            case 1002:  p1002();    break;
+            
+            default:    Console.Write("\nOpção Inválida\n");    break;
+        }
+
+    }while( prog != 0 );
+    //p1001();
+    //p1002();
